e54926dc83f8f35945944292584bdf5ec8c02c22
MHMD_Alaa <mhmdalaa333@gmail.com>
2019-06-21 01:31:42 +0200

    finished indexWebPage & SearchResult

--- b tree/src/eg/edu/alexu/csd/filestructure/btree/implementation/SearchEngine.java
+++ b tree/src/eg/edu/alexu/csd/filestructure/btree/implementation/SearchEngine.java
@@ -1,15 +1,75 @@
 package eg.edu.alexu.csd.filestructure.btree.implementation;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.management.RuntimeErrorException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import eg.edu.alexu.csd.filestructure.btree.IBTree;
 import eg.edu.alexu.csd.filestructure.btree.ISearchEngine;
 import eg.edu.alexu.csd.filestructure.btree.ISearchResult;
+import javafx.util.Pair;
 
 public class SearchEngine implements ISearchEngine {
+	IBTree<String, List<ISearchResult>> bTree;
+
+	public SearchEngine(int t) {
+		bTree = new BTreeImp<String, List<ISearchResult>>(t);
+	}
 
 	@Override
 	public void indexWebPage(String filePath) {
-		// TODO Auto-generated method stub
+		List<Pair<String, String>> docArray = parseXML(filePath);
+		Pattern p = Pattern.compile("\\w+");
+		for (Pair<String, String> doc : docArray) {
+			String id = doc.getKey();
+			Matcher m = p.matcher(doc.getValue());
+			List<String> words = new ArrayList<>();
+			while (m.find()) {
+				words.add(m.group());
+			}
+			Map<String, Integer> results = new HashMap<>();
+			for (String word : words) {
+				String lowerCaseWord = word.toLowerCase();
+				results.put(lowerCaseWord, results.getOrDefault(lowerCaseWord, 0) + 1);
+			}
+			for (Map.Entry<String, Integer> wordRankPair : results.entrySet()) {
+				List<ISearchResult> searchResultList;
+				searchResultList = bTree.search(wordRankPair.getKey());
+				if (searchResultList != null) {
+					boolean docIndexedBefore = false;
+					for (ISearchResult searchResult : searchResultList) {
+						if (id.equals(searchResult.getId())) {
+							docIndexedBefore = true;
+							searchResult.setRank(wordRankPair.getValue());
+							break;
+						}
+					}
+					if (!docIndexedBefore) {
+						searchResultList.add(new SearchResult(id, wordRankPair.getValue()));
+					}
+
+				} else {
+					searchResultList = new ArrayList<>();
+					searchResultList.add(new SearchResult(id, wordRankPair.getValue()));
+					bTree.insert(wordRankPair.getKey(), searchResultList);
+				}
+			}
+		}
 
 	}
 
@@ -35,6 +95,39 @@ public class SearchEngine implements ISearchEngine {
 	public List<ISearchResult> searchByMultipleWordWithRanking(String sentence) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * Read XML file with DOM parser
+	 * 
+	 * @param filePath
+	 * @return List of pair contains <id, text> for every document in the file
+	 */
+	private List<Pair<String, String>> parseXML(String filePath) {
+		List<Pair<String, String>> docArray = new ArrayList<>();
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder docBuilder = factory.newDocumentBuilder();
+			Document file = docBuilder.parse(filePath);
+			NodeList docList = file.getElementsByTagName("doc");
+			for (int i = 0; i < docList.getLength(); i++) {
+				Node docNode = docList.item(i);
+				if (docNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element docElement = (Element) docNode;
+					docArray.add(new Pair<String, String>(docElement.getAttribute("id"), docElement.getTextContent()));
+				}
+			}
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			e.printStackTrace();
+			throw new RuntimeErrorException(null);
+		}
+		return docArray;
+	}
+
+	public static void main(String[] arg) {
+		SearchEngine x = new SearchEngine(100);
+		x.indexWebPage("res\\wiki_00");
+		System.out.println();
 	}
 
 }
